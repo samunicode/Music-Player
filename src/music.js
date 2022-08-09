3301561d30a8237264841ba7d63752440ff83bf5
Sameer Chauhan <91241984+samunicode@users.noreply.github.com>
2022-08-10 01:12:40 +0530

    Add files via upload

---
+++ src/music.js
@@ -0,0 +1,71 @@
+let allMusic = [
+  {
+    name: "Heat Waves - Dreamland",
+    artist: "Glass Animals",
+    img: "HeatWaves",
+    src: "HeatWaves"
+  },
+  {
+    name: "Cold Heart - PNAU Remix",
+    artist: "Elton John; Dua Lipa; PNAU",
+    img: "ColdHeart",
+    src: "ColdHeart"
+  },
+  
+  {
+    name: "Running Up That Hill",
+    artist: "Kate Bush",
+    img: "RunningUpThatHill",
+    src: "RunningUpThatHill"
+  },
+  
+  {
+    name: "Bad Habits",
+    artist: "Ed Shreeran",
+    img: "BadHabits",
+    src: "BadHabits"
+  },
+  {
+    name: "Infinity - Feel Something",
+    artist: "Jaymes Young",
+    img: "Infinity",
+    src: "Infinity"
+  },
+  {
+    name: "Beautiful Mistakes",
+    artist: "Maroon 5, Megan Thee Stallion",
+    img: "BeautifulMistakes",
+    src: "BeautifulMistakes"
+  },
+  {
+    name: "Chained To The Rhythm",
+    artist: "Katy Perry, Skip Marley",
+    img: "ChainedToTheRhythm",
+    src: "ChainedToTheRhythm"
+  },
+  {
+    name: "Con Calma",
+    artist: "Daddy Yankee, Katy Perry, Snow",
+    img: "ConCalma",
+    src: "ConCalma"
+  },
+  {
+    name: "INDUSTRY BABY (feat. Jack Harlow)",
+    artist: "Lil Nas X, Jack Harlow",
+    img: "INDUSTRYBABY",
+    src: "INDUSTRYBABY"
+  },
+  {
+    name: "Swish Swish",
+    artist: "Katy Perry, Nicki Minaj",
+    img: "SwishSwish",
+    src: "SwishSwish"
+  },
+
+  {
+    name: "1, 2, 3",
+    artist: "Sofia Reyes, Jason Derulo, De La Ghetto",
+    img: "1_2_3",
+    src: "1_2_3"
+  }
+]
